c660c8191fd14c7784c4d7bb092f9215ed9d1bc1
Moritz Kalteyer <92576548+MoritzKal@users.noreply.github.com>
2025-09-01 00:08:07 +0200

    chore: move DB migrations and tests to app module

--- api/api-app/src/test/java/com/chessapp/db/DbJdbcTestBase.java
+++ api/api-app/src/test/java/com/chessapp/db/DbJdbcTestBase.java
@@ -1,6 +1,7 @@
 package com.chessapp.db;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
