418a3d851d245ef0af1f0ddd7dd42908554d2df0
Moritz Kalteyer <92576548+MoritzKal@users.noreply.github.com>
2025-09-01 18:57:33 +0200

    test: activate test profile for JDBC tests

--- api/api-app/src/test/java/com/chessapp/db/DbJdbcTestBase.java
+++ api/api-app/src/test/java/com/chessapp/db/DbJdbcTestBase.java
@@ -2,7 +2,9 @@ package com.chessapp.db;
 
 import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public abstract class DbJdbcTestBase {}
